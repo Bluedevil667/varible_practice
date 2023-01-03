3898227daa3082fcb95412a5081e83c21ccc70c5
Logan Wilson <loganwilson777@gmail.com>
2023-01-03 15:23:03 -0700

    reassigned 2 variables with puts

--- varibles.rb
+++ varibles.rb
@@ -1,4 +1,4 @@
-email = "loganwilson777@gmail.com"
+email = "loganwilson77@gmail.com"
 name = "Logan"
 age = 19
 birthday = 06/17/2003
@@ -6,3 +6,9 @@ account_balance = 1200.00
 battery_percentage = 97.3
 tired = true
 exited = true
+email = "loganwilson777@gmail.com"
+name = "logan w"
+puts email
+puts name
+puts account_balance
+puts birthday
