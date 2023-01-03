adef8a2c9b95a0292917d40ae3371757bef36cd4
Logan Wilson <loganwilson777@gmail.com>
2023-01-03 15:25:39 -0700

    fixed spelling error

--- varibles.rb
+++ varibles.rb
@@ -5,7 +5,7 @@ birthday = 06/17/2003
 account_balance = 1200.00
 battery_percentage = 97.3
 tired = true
-exited = true
+excited = true
 email = "loganwilson777@gmail.com"
 name = "logan w"
 puts email
